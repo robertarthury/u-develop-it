bd46b9c75c32cd871028fc6f968f5943336a6d0a
robertarthury <robertarthury@gmail.com>
2022-04-30 11:50:04 -0400

    finally got it to work

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const res = require('express/lib/response');
+
+const PORT  = process.env.PORT || 3001;
+const app = express();
+// middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//get or post goes here man
+
+// (Not found response)
+app.use((req,res) => {
+    res.status(404).end();
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
